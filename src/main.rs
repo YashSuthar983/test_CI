c196d9897b909f314900bfe9aee31dd6bbf6324e
Piyush1045 <piyushgeh20@gmail.com>
2025-10-27 00:37:48 +0530

    dsgsf

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@
 fn main() {
     println!("Hello, CI fmt test!");
     let mut numbers = vec![3, 1, 2];
-    for n in numbers.iter_mut() {
-        *n += 1;
-        println!("{}", n);
+    for n in numbers.iter_mut() {*n += 1;println!("{}", n);
     }
     let sum: i32 = numbers.iter().copied().sum();
     println!("sum={}", sum)
