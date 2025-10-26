2f77fd99a7cfa530a62483d411911d0f965868dc
Piyush1045 <185327594+Piyush1045@users.noreply.github.com>
2025-10-26 19:08:12 +0000

    chore(fmt): apply cargo fmt --all

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@
 fn main() {
     println!("Hello, CI fmt test!");
     let mut numbers = vec![3, 1, 2];
-    for n in numbers.iter_mut() {*n += 1;println!("{}", n);
+    for n in numbers.iter_mut() {
+        *n += 1;
+        println!("{}", n);
     }
     let sum: i32 = numbers.iter().copied().sum();
     println!("sum={}", sum)
